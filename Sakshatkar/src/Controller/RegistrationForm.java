571cedaeaeb0bbcc74539e937123233a5684ba99
Manas Uniyal <manasuniyal100@gmail.com>
2019-09-01 00:38:16 +0530

    Added functionality of Register and Select Image button

--- Sakshatkar/src/Controller/RegistrationForm.java
+++ Sakshatkar/src/Controller/RegistrationForm.java
@@ -16,6 +16,11 @@ import javafx.stage.Stage;
 import javax.xml.soap.Text;
 import java.io.File;
 import java.net.MalformedURLException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.regex.Pattern;
+
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.sym.PATTERN;
 
 public class RegistrationForm {
 
@@ -40,17 +45,85 @@ public class RegistrationForm {
     @FXML
     Label error;
 
+    private void setErrorLabel(String str){
+        error.setText(str);
+    }
+
+    private static final String PASSWORD_PATTERN ="((?=.*[a-z])(?=.*d)(?=.*[@#$%])(?=.*[A-Z]).{6,16})";
+    private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    private static final String PHONENO_PATTERN = "\\d{10}";
+
     SqlQueryExecuter sqlQueryExecuter = new SqlQueryExecuter("root","","jdbc:mysql://localhost/sakshatkar");
 
-    public void register(MouseEvent event) {
-        if(userName.getText().isEmpty()){
-            error.setText("Enter user Name");
+    public void register(ActionEvent event) throws SQLException {
+
+        String userNameText = userName.getText();
+        if(userNameText.isEmpty()){
+            setErrorLabel("User Name cannot be empty");
             return;
         }
-        if(userID.getText().isEmpty()){
-            error.setText("Enter user ID ");
-            sqlQueryExecuter.
+
+        String userIDText = userID.getText();
+        boolean isValid = true;
+        if(userIDText.isEmpty()) {
+            setErrorLabel("User ID cannot be empty");
+            return;
         }
+        else{
+            error.setText("Enter user ID ");
+            ResultSet rs = sqlQueryExecuter.select("SELECT userID FROM user");
+            while(rs.next()){
+                String userID = rs.getString("userID");
+                if(userID.equals(userIDText)){
+                    isValid = false;
+                    break;
+                }
+            }
+        }
+        if(isValid == false) {
+            setErrorLabel("User ID already exists. ");
+            return;
+        }
+
+        String passwordText = password.getText();
+        if(passwordText.isEmpty()){
+            setErrorLabel("Password cannot be empty");
+            return;
+        }
+        else{
+            if(Pattern.compile(PASSWORD_PATTERN, Pattern.CASE_INSENSITIVE).matcher(passwordText).matches() == false){
+                setErrorLabel("Invalid password entered");
+                return;
+            }
+        }
+
+        String emailText = email.getText();
+        if(emailText.isEmpty()){
+            setErrorLabel("Email ID cannot be empty");
+            return;
+        }
+        else{
+            if(Pattern.compile(EMAIL_PATTERN, Pattern.CASE_INSENSITIVE).matcher(emailText).matches() == false){
+                setErrorLabel("Invalid email entered");
+                return;
+            }
+        }
+
+        String phoneNoText = phoneNo.getText();
+        if(phoneNoText.isEmpty()){
+            setErrorLabel("Phone number cannot be empty");
+            return;
+        }
+        else{
+            if(Pattern.compile(PHONENO_PATTERN, Pattern.CASE_INSENSITIVE).matcher(phoneNoText).matches() == false){
+                setErrorLabel("Invalid phone number");
+                return;
+            }
+        }
+
+        error.setText("");
+        RegisterData registerData = new RegisterData(userNameText, userIDText, passwordText, emailText, phoneNoText);
+        
     }
 
     public void loadImage(ActionEvent event) {
@@ -64,4 +137,5 @@ public class RegistrationForm {
             e.printStackTrace();
         }
     }
+
 }
