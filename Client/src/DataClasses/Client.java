c7616f2cb567b463e4c267a6efd7cd2cec008429
Sahaj-Bamba <sahajbamba1999@gmail.com>
2019-09-03 00:45:58 +0530

    Pic part still pending now moving on to Dashboard part

--- Client/src/DataClasses/Client.java
+++ Client/src/DataClasses/Client.java
@@ -1,14 +1,17 @@
 package DataClasses;
 
+import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
 
+import java.awt.image.BufferedImage;
 import java.sql.Timestamp;
 
 public class Client {
 
 	String name;
 	int isOnline;
-	Image pic;
+	BufferedImage pic;
 	Timestamp lastOnline;
 	String id;
 	int status;
@@ -22,6 +25,9 @@ public class Client {
 	}
 
 	public Image getPic() {
+		Image pic = null;
+//		pic = new Image(SwingFXUtils.toFXImage(this.pic,null));
+//		pic = new Image()
 		return pic;
 	}
 
@@ -40,12 +46,10 @@ public class Client {
 	public Client(String name, int isOnline, Image pic, Timestamp lastOnline, String id, int status) {
 		this.name = name;
 		this.isOnline = isOnline;
-		this.pic = pic;
+		this.pic = SwingFXUtils.fromFXImage(pic, null);
 		this.lastOnline = lastOnline;
 		this.id = id;
 		this.status = status;
 	}
-
-
 
 }
